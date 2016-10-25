317d778dfb064209e371fa36b802eb2b1214790a
Aditya Beta Sutopo Putra <Aditya Beta Sutopo Putra>
2016-10-25 11:31:17 +0700

    Initialize

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject pickup;
+	public Vector3 spawnValues;
+
+	private float lifetime = 5.0f;
+	
+	void Start() {
+		InvokeRepeating("Spawn", 0.0f, 6.0f);
+	}
+
+	void Spawn() {
+		for (int i = 0; i < 12; i++)
+			SpawnPickup();
+	}
+
+	void SpawnPickup() {
+		Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x,spawnValues.x),Random.Range(-spawnValues.y,spawnValues.y),spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
+		Destroy (Instantiate (pickup, spawnPosition, spawnRotation), lifetime);
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+	public float fireRate;
+	public GameObject bomb;
+	public Transform bombObject;
+
+	private float nextFire;
+	private Rigidbody2D rb2d;
+	private int count;
+
+	void Start() {
+		rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate (){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive (false);
+			count += 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win";
+		}
+	}
+
+	void Update() {
+		if (Input.GetButton("Jump") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			//GameObject clone = 
+			Instantiate (bomb, bombObject.position, bombObject.rotation);// as GameObject;
+		}
+	}
+
+}
