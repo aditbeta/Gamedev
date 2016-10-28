c8e48f54b86a97b39294eedd16ac216b1b69b873
Aditya Beta Sutopo Putra <Aditya Beta Sutopo Putra>
2016-10-28 18:33:31 +0700

    yaela

--- Assets/Scripts/BombPrefabScript.cs
+++ Assets/Scripts/BombPrefabScript.cs
@@ -6,16 +6,21 @@ public class BombPrefabScript : MonoBehaviour {
 	private float timeCounter;
 	private float timeStopper = 1;
 
+
+
 	// Use this for initialization
 	void Start () {
-	
 	}
-	
+
+
+
 	// Update is called once per frame
 	void Update () {
 		timeCounter += Time.deltaTime;
 		if (timeCounter >= timeStopper) {
 			GetComponent<Rigidbody2D> ().velocity = Vector3.zero;
+//			GetComponent<Explosion>().Explode ();
+			Destroy (gameObject, .3f);
 		}
 	}
 }
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//buat nampilin sprite ledakan
+public class Explosion : MonoBehaviour {
+
+	public GameObject explosionPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void Explode() {
+		Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+		//		GetComponent<MeshRenderer>().enabled = false;
+		//		transform.FindChild("Collider").gameObject.SetActive(false);
+		Destroy(gameObject, .3f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
